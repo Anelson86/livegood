5e6680e5e1d25bd5dea7614623e7e145665870b3
BaoulyNelson <elconquistadorbaoulyn@gmail.com>
2024-02-07 22:36:22 -0500

    Supprimer tous les fichiers du dépôt

--- SCRIPT/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.menu-icon').addEventListener('click', function() {
-    document.querySelector('.side-menu').style.left = '0';
-});
-
-document.querySelector('.close-icon').addEventListener('click', function() {
-    document.querySelector('.side-menu').style.left = '-250px';
-});
